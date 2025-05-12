a8664c1cf45b8c93a748ab58328080812a20878b
Arenas07 <189859279+Arenas07@users.noreply.github.com>
2025-05-12 10:22:48 -0500

    Array basico

---
+++ Sintaxis_basica/clase_6/indexArray.php
@@ -0,0 +1,11 @@
+<?php
+
+
+// Creamos un arreglo llamado $nombres con tres elementos iniciales
+$nombres = ["Viktor", "Juan", "Pedro"];
+
+// Agregamos un nuevo nombre ("Ana") al final del arreglo
+$nombres[] = "Ana";
+
+// Mostramos el cuarto elemento del arreglo (índice 3), que es "Ana"
+echo $nombres[3];
